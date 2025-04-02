d73178dafd9b7aa886bfc7b7b14f349597f0cea1
GlebChiz <gchyz@jsdaddy.io>
2025-04-02 11:15:06 +0300

    feat: add no-restricted-imports rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
 import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
 
 const eslintConfig = [
   ...compat.config({
     extends: ['next', 'next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['./*', '../*'],
+        },
+      ],
+    },
   }),
 ]
 
